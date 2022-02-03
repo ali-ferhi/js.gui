b5a56ae920976d39f2003230c8c201ad70e354c0
Ali FERHI <ali.ferhi@hotmail.fr>
2022-02-03 20:28:28 +0100

    code-maze - part 2

--- ng-alpha/src/app/mat/code-maze/code-maze-routing.module.ts
+++ ng-alpha/src/app/mat/code-maze/code-maze-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RootComponent } from './root/root.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path:'', component: RootComponent, children: [
-    { path: '**', redirectTo: '' }
+    { path: 'home', component: HomeComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
   ] }
 ];
 
--- ng-alpha/src/app/mat/code-maze/code-maze.module.ts
+++ ng-alpha/src/app/mat/code-maze/code-maze.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { CodeMazeRoutingModule } from './code-maze-routing.module';
 import { RootComponent } from './root/root.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     RootComponent,
     LayoutComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     CodeMazeRoutingModule
   ]
 })
