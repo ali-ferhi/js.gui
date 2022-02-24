7bd2c0f3734451ea2b71ef7db3d36d66dc4afdeb
Ali FERHI <ali.ferhi@hotmail.fr>
2022-02-24 11:19:40 +0100

    init project

---
+++ src/app/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { RootComponent } from './root/root.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    RootComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    AppRoutingModule
+  ],
+  bootstrap: [RootComponent]
+})
+export class AppModule {}
